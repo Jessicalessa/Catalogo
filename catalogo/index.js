03cefe4291cbb14910b70f119becfbcaa87a08fb
Jéssica Estevam Lessa <93428392+Jessicalessa@users.noreply.github.com>
2024-01-24 11:12:55 -0300

    Add files via upload

---
+++ catalogo/index.js
@@ -0,0 +1,30 @@
+var comentarios=[]
+
+document.querySelector('input[type=submit]')
+.addEventListener('click',()=>{
+    var nomeProduto = document.querySelector('input[name=Text]');
+
+    items.push({
+        nome: Text.value
+    });
+
+    let Text = document.querySelector('.lista-comentarios');
+    Text.innerHTML = "";
+    items.map(function(val){
+
+        Text.innerHTML+=`
+        <div class="lista-produto-single">
+        <h3>`+val.nome+`</h3>
+        </div>
+        `;
+    })
+    Text.value = "";
+    
+});
+
+document.querySelector('button[name=limpar]')
+.addEventListener('click',()=>{
+    items = [];
+    
+    document.querySelector('.lista-comentarios').innerHTML = "";
+})
