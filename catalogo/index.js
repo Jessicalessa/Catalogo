bf282e89cae9db8c0eb490f930efa3811ebb2eb1
Jéssica Estevam Lessa <93428392+Jessicalessa@users.noreply.github.com>
2024-05-09 17:00:35 -0300

    Update index.js

--- catalogo/index.js
+++ catalogo/index.js
@@ -4,7 +4,6 @@ document.querySelector('input[type=submit]')
 .addEventListener('click',()=>{
     var nomeProduto = document.querySelector('input[name=Text]');
 
-
     items.push({
         nome: Text.value
     });
