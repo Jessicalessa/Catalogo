c45132e8288ab85220ed812a13003310057a6999
Jéssica Estevam Lessa <93428392+Jessicalessa@users.noreply.github.com>
2024-05-09 17:00:19 -0300

    Update index.js

--- catalogo/index.js
+++ catalogo/index.js
@@ -3,6 +3,7 @@ var comentarios=[]
 document.querySelector('input[type=submit]')
 .addEventListener('click',()=>{
     var nomeProduto = document.querySelector('input[name=Text]');
+    
 
     items.push({
         nome: Text.value
